3a3f54f309cbe3ab87537bfd6bd21bdde9c28f49
Yuzar Aryadi <Yuzar.Aryadi@outlook.com>
2023-12-25 11:44:29 +0700

    Update property name to be similar with its model link

--- PoolingEngine.Domain/Entities/TagValue.cs
+++ PoolingEngine.Domain/Entities/TagValue.cs
@@ -8,8 +8,8 @@ namespace PoolingEngine.Domain.Entities
         public int Id { get; set; }
         [Required]
         public string RequestItemId { get; set; } = string.Empty;
-        public int DeviceId { get; set; }
-        public int TagId { get; set; }
+        public int DeviceItemId { get; set; }
+        public int TagItemId { get; set; }
         [MaxLength(20)]
         public string? Value { get; set; }
         public DateTime TimeStamp { get; set; }
