df7e7ce874e55062ff3d4155928f2f7e6cdd5477
Yuzar Aryadi <Yuzar.Aryadi@outlook.com>
2023-12-27 22:27:08 +0700

    Add data seeder

--- PoolingEngine.API/Program.cs
+++ PoolingEngine.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using PoolingEngine.API.Helper;
 using PoolingEngine.DataAccess.Context;
 using PoolingEngine.DataAccess.Implementation;
 using PoolingEngine.Domain.Repository;
@@ -12,7 +13,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddTransient<DataSeeder>();
 //Add Entity Framework
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"))
@@ -23,6 +24,17 @@ builder.Services.AddTransient<IUnitOfWork,UnitOfWork>();
 
 var app = builder.Build();
 
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
+    SeedData(app);
+void SeedData(IHost app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<DataSeeder>();
+        service.Seed();
+    }
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
