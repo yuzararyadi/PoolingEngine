3e7bc1b3bcd058d5dacf68944b1c2d838cd31bdb
Yuzar Aryadi <Yuzar.Aryadi@outlook.com>
2023-12-27 22:25:06 +0700

    Modify the dto to include child link

--- PoolingEngine.API/Dtos/DeviceItemDto.cs
+++ PoolingEngine.API/Dtos/DeviceItemDto.cs
@@ -6,6 +6,7 @@ namespace PoolingEngine.API.Dtos
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
+        public IEnumerable<TagGroupDto> TagGroups { get; set; } = new HashSet<TagGroupDto>();
 
     }
 }
--- PoolingEngine.API/Dtos/TagValueDto.cs
+++ PoolingEngine.API/Dtos/TagValueDto.cs
@@ -5,7 +5,7 @@ namespace PoolingEngine.API.Dtos
     public class TagValueDto
     {
         public int Id { get; set; }
-        public string RequestItemId { get; set; }
+        public string RequestItemId { get; set; } = string.Empty;
         public int DeviceId { get; set; }
         public int TagId { get; set; }
         public string? Value { get; set; }
