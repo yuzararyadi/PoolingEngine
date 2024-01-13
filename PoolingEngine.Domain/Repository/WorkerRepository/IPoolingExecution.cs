89a07fbfc927f25b74fd6a187367aea23b4e7ce9
Yuzar Aryadi <Yuzar.Aryadi@outlook.com>
2024-01-13 17:12:45 +0700

    part of worker services

---
+++ PoolingEngine.Domain/Repository/WorkerRepository/IPoolingExecution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoolingEngine.Domain.Repository.WorkerRepository
+{
+    public interface IPoolingExecution
+    {
+        Task DoRequestItemPooling(CancellationToken stoppingToken);
+    }
+}
