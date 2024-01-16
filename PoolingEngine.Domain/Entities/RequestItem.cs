bee8d6f7c3e6f556bf9f48dcf9f0326749a48253
Yuzar Aryadi <Yuzar.Aryadi@outlook.com>
2024-01-16 22:14:36 +0700

    put DeviceItemId as required in requestItem model

--- PoolingEngine.Domain/Entities/RequestItem.cs
+++ PoolingEngine.Domain/Entities/RequestItem.cs
@@ -9,7 +9,7 @@ namespace PoolingEngine.Domain.Entities
         public Guid Id { get; set; } = Guid.NewGuid();
         public string RequestPoolingId { get; set; } = string.Empty;
         public EnumPoolingPriority Priority { get; set; } = EnumPoolingPriority.LOW;
-        public int? DeviceItemId { get; set; }
+        public int DeviceItemId { get; set; }
         //public IEnumerable<int> TagGroupsIds { get; set; } = new List<int>();
         public DateTime TimeStamp { get; set; } = DateTime.Now;
         public IEnumerable<TagGroup> TagGroups { get; set; } = new HashSet<TagGroup>();
