066ff7ec0eb1ad8993083010f2e5d018ea13702c
Yuzar Aryadi <Yuzar.Aryadi@outlook.com>
2024-01-16 22:10:20 +0700

    Add missing get set on ienumerable tagitems

--- PoolingEngine.API/Dtos/TagGroupDto.cs
+++ PoolingEngine.API/Dtos/TagGroupDto.cs
@@ -6,6 +6,6 @@ namespace PoolingEngine.API.Dtos
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public IEnumerable<TagItemDto> TagItems = new HashSet<TagItemDto>();
+        public IEnumerable<TagItemDto> TagItems { get; set; } = new HashSet<TagItemDto>();
     }
 }
