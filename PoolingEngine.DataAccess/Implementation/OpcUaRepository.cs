98ddfcc3d750df5dca16c4426fed08847d686782
Yuzar Aryadi <Yuzar.Aryadi@outlook.com>
2024-01-16 22:14:08 +0700

    Add interface and repository for OPC UA operation

---
+++ PoolingEngine.DataAccess/Implementation/OpcUaRepository.cs
@@ -0,0 +1,69 @@
+﻿using PoolingEngine.DataAccess.Context;
+using PoolingEngine.Domain.Entities;
+using PoolingEngine.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Opc.UaFx.Client;
+using Opc.UaFx;
+using System.Net.Sockets;
+
+namespace PoolingEngine.DataAccess.Implementation
+{
+    public class OpcUaRepository : IOpcUaRepository
+    {
+        private OpcClient _opcClient;
+        
+        public OpcUaRepository()
+        {
+            _opcClient = new OpcClient("opc.tcp://SG9780105889137:49320");
+            _opcClient.Security.UserIdentity = new OpcClientIdentity("yuzar", "yuzararyadiadmin");
+        }
+        private void OpcUAConnect()
+        {
+            _opcClient.Connect();
+            if(!(_opcClient.State == OpcClientState.Connected || _opcClient.State == OpcClientState.Reconnected))
+            {
+                Task.Delay(5000).Wait();
+                OpcUAConnect();
+            }
+                
+        }
+        public IEnumerable<OpcValue>? OpcRead(DeviceItem deviceItem, List<TagDef> tagDefs)
+        {
+            try
+            {
+                if (!(_opcClient.State == OpcClientState.Connected || _opcClient.State == OpcClientState.Reconnected))
+                    OpcUAConnect();
+                List<OpcNodeId> nodeIds = new List<OpcNodeId>();
+                foreach (TagDef tagDef in tagDefs)
+                {
+                    nodeIds.Add(tagDef.MapAddress);
+                }
+                if (nodeIds.Count > 0)
+                {
+                    var value = _opcClient.ReadNodes(nodeIds);
+                    return value;
+                }
+                else return null;
+                
+            }
+            catch 
+            {
+                return null;
+            }
+            
+        }
+        public void OpcCommand()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpcWrite()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PoolingEngine.Domain/Repository/IOpcUaRepository.cs
@@ -0,0 +1,17 @@
+﻿using Opc.UaFx;
+using PoolingEngine.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoolingEngine.Domain.Repository
+{
+    public interface IOpcUaRepository
+    {
+        IEnumerable<OpcValue>? OpcRead(DeviceItem deviceItem, List<TagDef> tagDefs);
+        void OpcWrite();
+        void OpcCommand();
+    }
+}
