0ff6b7f9f0baa9b4013283c34929169653fadd65
Yuzar Aryadi <Yuzar.Aryadi@outlook.com>
2024-01-19 23:46:25 +0700

    Add forth migration of AppDbContext

--- PoolingEngine.DataAccess/Context/Migrations/20240119103638_ForthMigration.Designer.cs
+++ PoolingEngine.DataAccess/Context/Migrations/20240119103638_ForthMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PoolingEngine.DataAccess.Context;
 
@@ -11,9 +12,11 @@ using PoolingEngine.DataAccess.Context;
 namespace PoolingEngine.DataAccess.Context.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240119103638_ForthMigration")]
+    partial class ForthMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PoolingEngine.DataAccess/Context/Migrations/20240119103638_ForthMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PoolingEngine.DataAccess.Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class ForthMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RequestType",
+                table: "RequestItem",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RequestType",
+                table: "RequestItem");
+        }
+    }
+}
