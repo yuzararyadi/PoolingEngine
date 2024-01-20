8a163639a06c8693788dc2983126bbe191f2c666
Yuzar Aryadi <Yuzar.Aryadi@outlook.com>
2024-01-21 00:28:13 +0700

    Update data seeder for TagItem

--- PoolingEngine.API/Helper/DataSeeder.cs
+++ PoolingEngine.API/Helper/DataSeeder.cs
@@ -62,30 +62,79 @@ namespace PoolingEngine.API.Helper
                 {
                     new TagItem()
                     {
-                        Name = "SPM",
-                        Description = "Stroke Per Minute"
+                        Name = "Current SPM",
+                        Description = "Current SPM"
                     },
                     new TagItem()
                     {
-                        Name = "Today Runtime",
-                        Description = "Today Runtime"
+                        Name = "Average SPM",
+                        Description = "Average SPM"
                     },
                     new TagItem()
                     {
-                        Name = "Yest Runtime",
-                        Description = "Yesterday Runtime"
+                        Name = "Card Area",
+                        Description = "Card Area"
                     },
                     new TagItem()
                     {
-                        Name = "Stroke Length",
-                        Description = "Stroke Length"
+                        Name = "Daily Minimum Load",
+                        Description = "Daily Minimum Load"
+                    },
+                    new TagItem()
+                    {
+                        Name = "Daily Maximum Load",
+                        Description = "Daily Maximum Load"
+                    },
+                    new TagItem()
+                    {
+                        Name = "Current Cycle Runtime",
+                        Description = "Current Cycle Runtime"
+                    },
+                    new TagItem()
+                    {
+                        Name = "Run Time Today",
+                        Description = "Run Time Today"
+                    },
+                    new TagItem()
+                    {
+                        Name = "Run Time Yesterday",
+                        Description = "Run Time Yesterday"
+                    },
+                    new TagItem()
+                    {
+                        Name = "Cycles Today",
+                        Description = "Cycles Today"
+                    },
+                    new TagItem()
+                    {
+                        Name = "Cycles Yesterday",
+                        Description = "Cycles Yesterday"
+                    },
+                    new TagItem()
+                    {
+                        Name = "Average Cycle Time",
+                        Description = "Average Cycle Time"
+                    },
+                    new TagItem()
+                    {
+                        Name = "Inferred Production Today",
+                        Description = "Inferred Production Today"
+                    },
+                    new TagItem()
+                    {
+                        Name = "Inferred Production Yesterday",
+                        Description = "Inferred Production Yesterday"
+                    },
+                    new TagItem()
+                    {
+                        Name = "Current Pump Fillage",
+                        Description = "Current Pump Fillage"
                     },
                     new TagItem()
                     {
                         Name = "Average Pump Fillage",
                         Description = "Average Pump Fillage"
                     }
-
                 };
                 _unitOfWork.TagItem.AddRange(tagItems);
             }
@@ -96,7 +145,7 @@ namespace PoolingEngine.API.Helper
                 {
                     new TagGroup()
                     {
-                        Name = "WellPilot Type Well"
+                        Name = "Main Tag"
                     }
                 };
                 _unitOfWork.TagGroup.AddRange(tagGroups);
