4a4bd3a693a993f3429adc76c42db21313e63ec7
Yuzar Aryadi <Yuzar.Aryadi@outlook.com>
2024-01-21 00:31:57 +0700

    tag value never null since list. and cleanup trans comment

--- PoolingEngine.API/Worker/Worker.cs
+++ PoolingEngine.API/Worker/Worker.cs
@@ -25,55 +25,12 @@ namespace PoolingEngine.API.Worker
                 {
                     List<TagValue> tagValues = new List<TagValue>();
                     tagValues = _requestItemRepository.ExecuteRequestItem();
-                    if (tagValues != null)
+                    if (tagValues.Count > 0)
                     {
                         await _poolingBroadcastContext.Clients.All.SendAsync("PoolingResult", tagValues);
                     }
                 });
             }
         }
-
-        //private async Task DoWork()
-        //{
-        //    var requestItem = _requestItemRepository.RequestItems()
-        //       .OrderByDescending(x => x.Priority)
-        //       .ThenByDescending(x => x.TimeStamp).FirstOrDefault();
-        //    if (requestItem != null)
-        //    {
-        //        using (var scope = _serviceProvider.CreateScope())
-        //        {
-        //            var context = scope.ServiceProvider.GetService<IUnitOfWork>();
-        //            var deviceItem = context.DeviceItem.GetById(requestItem.DeviceItemId);
-                    
-        //            var listTagItems = context.TagGroup.LinkedTagItems(requestItem.TagGroups.ToList());
-        //            if (listTagItems.Count > 0 && deviceItem != null)
-        //            {
-        //                var listTagDef = context.TagDef.ListDeviceTagDef(deviceItem, listTagItems);
-        //                var poolResult = _opcUaRepository.OpcRead(deviceItem, listTagDef);
-        //                if (poolResult != null)
-        //                {
-        //                    List<TagValue> tagValues = new List<TagValue>();
-        //                    int i = 0;
-        //                    foreach (var value in poolResult)
-        //                    {
-        //                        TagValue tagValue = new TagValue()
-        //                        {
-        //                            RequestItemId = requestItem.Id.ToString(),
-        //                            DeviceItemId = requestItem.DeviceItemId,
-        //                            TagItemId = listTagItems[i].Id,
-        //                            Value = value.Value.ToString(),
-        //                            TimeStamp = (DateTime)value.SourceTimestamp
-
-        //                        };
-        //                        tagValues.Add(tagValue);
-        //                        i++;
-        //                    }
-        //                    await _poolingBroadcastContext.Clients.All.SendAsync("PoolingResult", tagValues);
-        //                }
-        //            }
-        //        }
-        //        _requestItemRepository.Remove(requestItem);
-        //    }
-        //}
     }
 }
